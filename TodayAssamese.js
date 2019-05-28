fdeb93476252e2a2c9b015d16fea60131fd9c845
দিব্য দত্ত <dibbyadut@gmail.com>
2019-05-28 12:15:31 +0530

    Update TodayAssamese.js

--- TodayAssamese.js
+++ TodayAssamese.js
@@ -1,5 +1,4 @@
-<script>
-var d = new Date();
+{var d = new Date();
 var dates = ["১৭", "১৮", "১৯", "২০", "২১", "২২", "২৩", "২৪", "২৫", "২৬", "২৭", "২৮", "২৯", "৩০", "৩১", "৩২", "১", "২", "৩", "৪", "৫", "৬", "৭", "৮", "৯", "১০", "১১", "১২", "১৩", "১৪", "১৫", "১৬"];
 document.getElementById("Date").innerHTML = dates[d.getDate()];
-</script>
+}

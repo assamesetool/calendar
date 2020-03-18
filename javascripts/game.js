c3d16f22a88b01df7e6802878f9c0971ae94bc6e
দিব্য দত্ত <dibbyadut@gmail.com>
2020-03-18 17:23:35 +0530

    Update game.js

--- javascripts/game.js
+++ javascripts/game.js
@@ -3,28 +3,21 @@ function dragStarted(evt) {
     source = evt.target;
     evt.dataTransfer.setData("text/plain", evt.target.innerHTML);
     evt.dataTransfer.effectAllowed = "move";
-    document.getElementById('uldPopup').style.display = "none";
-}
-
+    document.getElementById('uldPopup').style.display = "none";}
+	
 function draggingOver(evt) {
     evt.preventDefault();
-    evt.dataTransfer.dropEffect = "move";
-}
-
+    evt.dataTransfer.dropEffect = "move";}
+	
 function draggingLeave(evt) {
     evt.preventDefault();
-    evt.dataTransfer.dropEffect = "move";
-}
-function newgame() {
-    window.location.replace("../game.html");}
-function play() {
-        var audio = document.getElementById("audio");
-        audio.loop = true;
-        audio.play();}
+    evt.dataTransfer.dropEffect = "move";}
+	
 function dragplay() {
         var audio = document.getElementById("audiodrag");
         audio.loop = false;
         audio.play();}
+		
 function winplay() {
         var audio = document.getElementById("audiowin");
         audio.loop = false;
